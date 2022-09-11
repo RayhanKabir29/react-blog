6bb80938bacb6cf251b9e2b7796030f805afa2db
Rayhan Kabir <rayhan.kabir29@gmail.com>
2022-09-12 00:45:49 +0600

    simple react-blog

---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20 ">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Welcome to my blog
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4 ">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis nisi
+        aut, labore asperiores assumenda libero praesentium voluptatum beatae
+        aperiam sequi tempore, maiores deleniti aliquid at ex vel, reprehenderit
+        commodi modi nemo est! Consectetur ipsum laboriosam quibusdam similique
+        sunt ea debitis, magnam pariatur atque magni alias dolor culpa quidem
+        natus veritatis?
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4 ">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis nisi
+        aut, labore asperiores assumenda libero praesentium voluptatum beatae
+        aperiam sequi tempore, maiores deleniti aliquid at ex vel, reprehenderit
+        commodi modi nemo est! Consectetur ipsum laboriosam quibusdam similique
+        sunt ea debitis, magnam pariatur atque magni alias dolor culpa quidem
+        natus veritatis?
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        blanditiis possimus quis dolor reiciendis error, vel in vitae numquam
+        eaque cumque placeat excepturi, repudiandae quos nisi. Quisquam fuga
+        commodi ipsum, magnam repellat nobis suscipit veritatis possimus sequi,
+        voluptatum molestias deserunt accusantium excepturi aperiam asperiores
+        ipsam aspernatur laboriosam mollitia? Fugit minus suscipit doloribus
+        labore ipsum sapiente consectetur illum itaque quibusdam est? Aliquid
+        tenetur, earum itaque veniam assumenda, magni dignissimos, eligendi hic
+        non vero quia cumque dicta? Consequatur corporis, dolores eligendi ullam
+        beatae quod in esse autem commodi ipsa pariatur amet debitis
+        perspiciatis aliquam voluptatibus expedita ipsam ut sapiente alias
+        doloribus nemo.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
